e92eeb7acb4ef8bb35392f4aa0a28c4f3157f7d8
shiva <shivamitabse@gmail.com>
2026-05-10 13:12:35 +0530

    cors-correction

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const apiRoutes = require('./routes/api');
-const { getDbConnection } = require('./db');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const apiRoutes = require("./routes/api");
+const { getDbConnection } = require("./db");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  optionsSuccessStatus: 200
+  origin:
+    process.env.FRONTEND_URL ||
+    "http://localhost:5173" ||
+    "https://www.testmysite.in",
+  optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -17,13 +20,13 @@ app.use(express.json());
 // Initialize Database on startup
 getDbConnection()
   .then(() => {
-    console.log('Database connected and initialized.');
+    console.log("Database connected and initialized.");
   })
   .catch((err) => {
-    console.error('Failed to initialize database:', err);
+    console.error("Failed to initialize database:", err);
   });
 
-app.use('/api', apiRoutes);
+app.use("/api", apiRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
